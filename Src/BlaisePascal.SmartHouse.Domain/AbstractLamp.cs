492a640ed71e112a93ac35f40a57122320ee2c59
AlessandroGozziBlaisePascal <alessandro.gozzi.stud@ispascalcomandini.it>
2025-10-31 09:35:06 +0100

    aggiunta classe EcoLamp e classe astratta AbstractLamp

---
+++ Src/BlaisePascal.SmartHouse.Domain/AbstractLamp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlaisePascal.SmartHouse.Domain
+{
+    public abstract class AbstractLamp
+    {
+        public abstract void TurnOn();
+
+
+        public abstract void TurnOff();
+
+
+        public abstract void TurnUpBrightness(int brightValue);
+
+
+        public abstract void TurnDownBrightness(int brightValue);
+        
+    }
+}
---
+++ Src/BlaisePascal.SmartHouse.Domain/EcoLamp.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlaisePascal.SmartHouse.Domain
+{
+    public class EcoLamp: AbstractLamp
+    {
+        public bool IsOn { get; private set; }
+        //INTENSITA' LUMINOSA
+        public int Brightness { get; private set; }
+
+        public int MaxBrightness { get; private set; }
+
+        public EcoLamp(int maxBrightness)
+        {
+            IsOn = false;
+            Brightness = 0;
+            MaxBrightness = maxBrightness;
+        }
+
+        public EcoLamp() : this(70) { }
+
+        public override void TurnOn()
+        {
+            if (!IsOn)
+            {
+                IsOn = true;
+                Brightness = 25;
+            }
+        }
+
+        public override void TurnOff()
+        {
+            if (IsOn)
+            {
+                IsOn = false;
+                Brightness = 0;
+            }
+        }
+
+        public override void TurnUpBrightness(int brightValue)
+        {
+            if (IsOn)
+            {
+                Brightness = Math.Min(MaxBrightness, Brightness + brightValue);
+            }
+
+
+        }
+
+        public override void TurnDownBrightness(int brightValue)
+        {
+            if (IsOn)
+            {
+                Brightness = Math.Max(1, Brightness - brightValue);
+
+            }
+        }
+
+        public void ChangeMaxBrightness(int newMaxBrightness)
+        {
+            MaxBrightness = Math.Max(1, newMaxBrightness);
+            MaxBrightness = Math.Min(90, newMaxBrightness);
+        }
+
+    }
+}
