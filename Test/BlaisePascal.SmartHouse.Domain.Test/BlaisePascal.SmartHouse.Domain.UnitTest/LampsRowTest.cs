63d2baa19013504aebde8e0cd69380602f4365df
Francesco-Corbara <francesco.corbara.stud@ispascalcomandini>
2025-11-12 10:55:54 +0100

    Started LampsRow Test

--- Test/BlaisePascal.SmartHouse.Domain.Test/BlaisePascal.SmartHouse.Domain.UnitTest/LampsRowTest.cs
+++ Test/BlaisePascal.SmartHouse.Domain.Test/BlaisePascal.SmartHouse.Domain.UnitTest/LampsRowTest.cs
@@ -8,5 +8,18 @@ namespace BlaisePascal.SmartHouse.Domain.UnitTest
 {
     public class LampsRowTest
     {
+        LampsRow lampsRow = new LampsRow();
+        List<AbstractLamp> lampList = new List<AbstractLamp>();
+        AbstractLamp lamp = new Lamp();
+        EcoLamp ecoLamp = new EcoLamp();
+
+        [Fact]
+
+        public void LampsRow_AddLamp_WhenIAddListNotempty()
+        {
+            lampsRow.AddLamp(lamp);
+            Assert.Equal();
+        }
+
     }
 }
