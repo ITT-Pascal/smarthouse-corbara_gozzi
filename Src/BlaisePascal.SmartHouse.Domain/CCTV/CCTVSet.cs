2b2b4386781280fce4777d59c6276fa0f516f759
AlessandroGozziBlaisePascal <alessandro.gozzi.stud@ispascalcomandini.it>
2025-11-24 17:14:10 +0100

    added adminpassword in methods

--- Src/BlaisePascal.SmartHouse.Domain/CCTV/CCTVSet.cs
+++ Src/BlaisePascal.SmartHouse.Domain/CCTV/CCTVSet.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 using BlaisePascal.SmartHouse.Domain.LAMP;
 
 namespace BlaisePascal.SmartHouse.Domain.CCTV
@@ -33,9 +34,13 @@ namespace BlaisePascal.SmartHouse.Domain.CCTV
         {
             CCTVset.Insert(pos, camera);
         }
-        public void RemoveCCTV(int pos)
+        public void RemoveCCTV(int pos, string adminPassword)
         {
-            CCTVset.RemoveAt(pos);
+            if (AdminPassword == adminPassword)
+                CCTVset.RemoveAt(pos);
+            else
+                throw new ArgumentException("Password errata");
+                
         }
         private int GetPositionOfCCTV(Guid id)
         {
@@ -47,33 +52,41 @@ namespace BlaisePascal.SmartHouse.Domain.CCTV
             }
             return pos;
         }
-        public void RemoveCCTV(Guid id)
+        public void RemoveCCTV(Guid id, string adminPassword)
         {
-            CCTVset.Remove(CCTVset[GetPositionOfCCTV(id)]);
+            if (AdminPassword == adminPassword)
+                CCTVset.Remove(CCTVset[GetPositionOfCCTV(id)]);
+            else
+                throw new ArgumentException("Password errata");
+            
         }
-        public void RemoveCCTV(string name)
+        public void RemoveCCTV(string name, string adminPassword)
         {
-            for (int i = 0; i < CCTVset.Count; i++)
+            if (AdminPassword == adminPassword)
             {
-                if (CCTVset[i].CameraName == name)
-                    CCTVset.RemoveAt(i);
+                for (int i = 0; i < CCTVset.Count; i++)
+                {
+                    if (CCTVset[i].CameraName == name)
+                        CCTVset.RemoveAt(i);
+                }
             }
+            else
+            {
+                throw new ArgumentException("Password errata");
+            }
+            
         }
         public void SwitchOn()
         {
             for (int i = 0; i < CCTVset.Count; i++)
-            {
                 CCTVset[i].SwitchOnCCTV();
-            }
         }
         /// <summary>
         /// Accende telecamera in base all'ID
         /// </summary>
         /// <param name="guid"></param>
-        public void SwitchOn(Guid guid)
-        {
-            CCTVset[GetPositionOfCCTV(guid)].SwitchOnCCTV();
-        }
+        public void SwitchOn(Guid guid) { CCTVset[GetPositionOfCCTV(guid)].SwitchOnCCTV(); }
+       
         /// <summary>
         /// Accende telecamera in base al nome
         /// </summary>
@@ -83,38 +96,45 @@ namespace BlaisePascal.SmartHouse.Domain.CCTV
             for (int i = 0; i < CCTVset.Count; i++)
             {
                 if (CCTVset[i].CameraName == name)
-                {
                     CCTVset[i].SwitchOnCCTV();
-                }
             }
         }
-        public void SwitchOff()
+        public void SwitchOff(string adminPassword)
         {
-            for (int i = 0; i < CCTVset.Count; i++)
-            {
-                CCTVset[i].SwitchOffCCTV();
-            }
+            if (AdminPassword == adminPassword)
+                for (int i = 0; i < CCTVset.Count; i++)
+                    CCTVset[i].SwitchOffCCTV();
+            else
+                throw new ArgumentException("Password errata");
         }
         /// <summary>
         /// Spegne telecamera in base all'ID
         /// </summary>
         /// <param name="guid"></param>
-        public void SwitchOff(Guid guid)
+        public void SwitchOff(Guid guid, string adminPassword)
         {
-            CCTVset[GetPositionOfCCTV(guid)].SwitchOffCCTV();
+            if (AdminPassword == adminPassword)
+                CCTVset[GetPositionOfCCTV(guid)].SwitchOffCCTV();
+            else
+                throw new ArgumentException("Password errata");
         }
         /// <summary>
         /// Spegne telecamera in base al nome
         /// </summary>
         /// <param name="name"></param>
-        public void SwitchOff(string name)
+        public void SwitchOff(string name, string adminPassword)
         {
-            for (int i = 0; i < CCTVset.Count; i++)
+            if (AdminPassword == adminPassword)
             {
-                if (CCTVset[i].CameraName == name)
+                for (int i = 0; i < CCTVset.Count; i++)
                 {
-                    CCTVset[i].SwitchOffCCTV();
+                    if (CCTVset[i].CameraName == name)
+                        CCTVset[i].SwitchOffCCTV();
                 }
+            }
+            else
+            {
+                throw new ArgumentException("Password errata");
             }
         }
     }
